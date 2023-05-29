562b1e5187041a9383ed7991ffd0a7c12c9c330a
Arvind Kumar <arvind@earnnest.me>
2023-05-29 09:37:33 +0530

    updates

--- main.py
+++ main.py
@@ -6,12 +6,12 @@
 from common.websites import mp4mania,sermovies,dl11Sermovies
 
 
-#oldValue=9016
-#newValue=9016
-#mp4mania(oldValue,newValue,False)
+# oldValue=1001
+# newValue=9030
+# mp4mania(oldValue,newValue,True)
 
 #url="http://dl6.sermovie.xyz/Movie/2023/"
 #sermovies(url)
 
-url="http://dl11.sermovie.xyz/Movies/2023/"
-dl11Sermovies(url)
+# url="http://dl11.sermovie.xyz/Movies/2023/"
+# dl11Sermovies(url)
