768239d336318d1b3b6fdd45d5b323bfbbce4637
Arvind Kumar <arvind@earnnest.me>
2023-06-03 15:26:30 +0530

    updated

--- add.js
+++ add.js
@@ -1,43 +1,34 @@
-
-
-
-
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const url = "https://mytodo-api.cyclic.app/v1/mverse/add-many";
+// const url = "http://localhost:3000/v1/mverse/add-many";
 
 const movieArr = require("./data/tmdb.json");
-const jump=100
+const jump = 100;
 
+const add = async () => {
+  for (var i = 0; i < movieArr.length; i = i + jump) {
+    const chunk = movieArr.slice(i, i + jump);
+    console.log("loading...");
+    try {
+      const res = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(chunk),
+      });
+      const d = await res.json();
 
-const add=async ()=>{
-    for (var i = 0; i < movieArr.length; i=i+jump) {
-      const chunk=movieArr.slice(i,i+jump)
-      console.log("loading...")
-     try {
-       const res=await fetch(url,
-          {
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(chunk)
-          })
-       const d=await res.json()
-
-            if (d.success) {
-              console.log("\x1b[32m%s\x1b[0m","saved: ",chunk.length);
-            } else {
-              console.log("\x1b[31m","error while saving",res);
-            }
-     } catch (e) {
-       console.log("\x1b[31m","error while saving",e);
-          
-     }
-
-       
+      if (d.success) {
+        console.log("\x1b[32m%s\x1b[0m", "saved: ", chunk.length);
+      } else {
+        console.log("\x1b[31m", "error while saving", res);
+      }
+    } catch (e) {
+      console.log("\x1b[31m", "error while saving", e);
     }
-}
+  }
+};
 
-add()
-            
+add();
